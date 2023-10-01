e67574d72512d73ae70ffaaec042539725fbc4de
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-10-01 20:39:57 +0700

    memodifikasi isi folder game info

--- scricpt.js
+++ scricpt.js
@@ -1,4 +1,4 @@
-alert('website ini just for fun,kalo masih ada bug mohon dimaklumi karena sedang dalam pengembangan')
+alert('website ini just for fun,kalo masih ada bug mohon dimaklumi karena sedang dalam pengembangan \n ~IG : Baaa26_')
 
 let hero = document.querySelector('.hero .row')
 let body = document.body
