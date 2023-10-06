3866031018ff69629a289ebca847306c563781e9
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-10-06 10:14:44 +0700

    membuat dan menambahkan folder Mk shop

---
+++ Game info/scricpt.js
@@ -0,0 +1,7 @@
+let headMK = document.querySelector('section.sinopsis .sinopsisInfo .HeadMK');
+let sinopsisIsi = document.querySelector('section.sinopsis .sinopsisInfo p.sinopsisIsi')
+
+window.addEventListener('load', function() {
+    headMK.classList.add('muncul');
+    sinopsisIsi.classList.add('muncul');
+})
