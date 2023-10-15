ffe65db38325c4cb69211cc16a8eec73b91a45e3
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-10-15 14:16:21 +0700

    memodifikasi isi folder MK tournament

---
+++ Turnament/scricpt.js
@@ -0,0 +1,12 @@
+let judulT = document.querySelector('section.turnamentIsi .isiTurnamentH1');
+let isiT = document.querySelector('section.turnamentIsi .isiTurnamentP');
+let fase_grup = document.querySelector('section.turnamentIsi .faseGrup');
+let knockOut = document.querySelector('section.turnamentIsi .knockOut');
+
+
+window.addEventListener('load', function() {
+    judulT.classList.add('muncul');
+    isiT.classList.add('muncul');
+    fase_grup.classList.add('muncul');
+    knockOut.classList.add('muncul')
+})
