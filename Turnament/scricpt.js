f25d65c6d82f0ccb40ab65d85cc5d5efa359010a
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-10-15 23:15:19 +0700

    memodifikasi isi folder turnament

--- Turnament/scricpt.js
+++ Turnament/scricpt.js
@@ -1,16 +1,16 @@
-// let judulT = document.querySelector('section.turnamentIsi .isiTurnamentH1');
-// let isiT = document.querySelector('section.turnamentIsi .isiTurnamentP');
-// let fase_grup = document.querySelector('section.turnamentIsi .faseGrup');
-// let knockOut = document.querySelector('section.turnamentIsi .knockOut');
-// let joinT = document.querySelector('section.turnamentIsi .joinT');
+let judulT = document.querySelector('section.turnamentIsi .isiTurnamentH1');
+let isiT = document.querySelector('section.turnamentIsi .isiTurnamentP');
+let fase_grup = document.querySelector('section.turnamentIsi .faseGrup');
+let knockOut = document.querySelector('section.turnamentIsi .knockOut');
+let joinT = document.querySelector('section.turnamentIsi .joinT');
 
-// window.addEventListener('load', function() {
-//     judulT.classList.add('muncul');
-//     isiT.classList.add('muncul');
-//     fase_grup.classList.add('muncul');
-//     knockOut.classList.add('muncul')
-// })
+window.addEventListener('load', function() {
+    judulT.classList.add('muncul');
+    isiT.classList.add('muncul');
+    fase_grup.classList.add('muncul');
+    knockOut.classList.add('muncul')
+})
 
-// joinT.addEventListener('click', function() {
-//     alert(`Tidak ada turnament terbaru :)`)
-// })
+joinT.addEventListener('click', function() {
+    alert(`Tidak ada turnament terbaru :)`)
+})
