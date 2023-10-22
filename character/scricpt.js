7c60f6a298d5785d87205aead0e7d7a793b23a2e
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-10-23 00:07:19 +0700

    memodifikasi isi character

---
+++ character/scricpt.js
@@ -0,0 +1,65 @@
+//========== scorpion =========
+let VoiceScorpion = document.querySelector('.scorpionVoice')
+
+VoiceScorpion.addEventListener('click', function() {
+        let audio = new Audio('../voice/scorpion.mp3')
+        audio.play()
+    })
+    //========= akhir scorpion ============
+
+
+
+
+
+//======== sub zero =========
+let VoicesubZero = document.querySelector('.subzeroVoice');
+
+VoicesubZero.addEventListener('click', function() {
+    let audio = new Audio('../voice/subzero.mp3')
+    audio.play()
+});
+//========== akhir sub zero ======== 
+
+
+
+// ======= liu kang =======
+let Voiceliukang = document.querySelector('.liukangVoice');
+
+Voiceliukang.addEventListener('click', function() {
+    let audio = new Audio('../voice/liu kang.mp3')
+    audio.play()
+});
+// ========== akhir liu kang ==========
+
+
+
+// =========== kitana =============
+let VoiceKitana = document.querySelector('.kitanaVoice');
+
+VoiceKitana.addEventListener('click', function() {
+    let audio = new Audio('../voice/kitana.mp3')
+    audio.play()
+});
+// =========== akhir kitana ===========
+
+
+
+//======= shang tsung =========
+let VoiceShangtsung = document.querySelector('.shangtsungVoice');
+
+VoiceShangtsung.addEventListener('click', function() {
+    let audio = new Audio('../voice/shangSung.mp3')
+    audio.play()
+});
+// ====== akhir shang tsuung ============
+
+
+
+//======= Baraka =========
+let VoiceBaraka = document.querySelector('.barakaVoice');
+
+VoiceBaraka.addEventListener('click', function() {
+    let audio = new Audio('../voice/baraka.mp3')
+    audio.play()
+});
+// ====== akhir Baraka ============
